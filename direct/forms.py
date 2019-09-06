7919a48a31250472a50bd4ee939c6dfde2a25c81
Panelata <32406174+Panelata@users.noreply.github.com>
2019-09-06 10:01:20 +1000

    Added is seller checkbox
    Added is seller textbox

--- direct/forms.py
+++ direct/forms.py
@@ -4,5 +4,9 @@ from .models import Customer
 class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Customer
-        fields=["firstName", "lastName", "email", "password", "phoneNumber", "street", "suburb", "postcode", "state", "country"]
-        labels={"firstName":"First Name", "lastName":"Surname", "email":"Email", "password":"Password", "phoneNumber":"Phone Number", "street":"Street Name", "suburb":"Suburb", "postcode":"Postcode", "state":"State", "country":"Country"}
+        fields=["firstName", "lastName", "email", "password", "phoneNumber",
+        "street", "suburb", "postcode", "state", "country", "isSeller"]
+        labels={"firstName":"First Name", "lastName":"Surname", "email":"Email",
+        "password":"Password", "phoneNumber":"Phone Number",
+        "street":"Street Name", "suburb":"Suburb", "postcode":"Postcode",
+        "state":"State", "country":"Country", "isSeller":"Selling?"}
--- direct/views.py
+++ direct/views.py
@@ -11,7 +11,6 @@ def register(request):
         return HttpResponseRedirect('/admin/')
         #For now after the user hits submit, they will be redirected to the admin page.
         #We should make some sort of confirmation page, or maybe redirect them to the login page.
-        #extra comment ignore
 
     context={'form': form}
     return render(request, 'registration.html', {'form':form})
