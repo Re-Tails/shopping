4526049e3eca425665662701b651b8b2c7deeecd
Panelata <32406174+Panelata@users.noreply.github.com>
2019-09-27 09:36:43 +1000

    Updated logic to only allow sellers to view the page
    If the logged in user is not a seller, then they will be redirected to the login scree. We can update this to redirect themt o an error page in the future.

--- direct/views.py
+++ direct/views.py
@@ -3,6 +3,7 @@ from direct.forms import CustomerCreationForm, SellerCreationForm, ProductCreati
 from django.views.generic import TemplateView
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from direct.models import Seller
 
 def index(request):
     return render(request, 'index.html')
@@ -46,19 +47,20 @@ def profile(request):
     return render(request, 'profile.html')
 
 def addProduct(request):
-    #if request.user.is_authenticated and request.user.isSeller == true:
-    if request.user.is_authenticated:
-        if request.method == "POST":
-            form = ProductCreationForm(request.POST, request.FILES)
-            if form.is_valid():
-                form.save()
-                return redirect('login')
-            else:
-                messages.warning(request, 'Invalid information enterred')
-        else:
-            form = ProductCreationForm()
-    else:
+    if not request.user.is_authenticated:
         return redirect('login')
+    for seller in Seller.objects.all():
+        if seller.pk != request.user.pk:
+            return redirect('login')
+    if request.method == "POST":
+        form = ProductCreationForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+        else:
+            messages.warning(request, 'Invalid information enterred')
+    else:
+        form = ProductCreationForm()
     context = {
         'form': form
     }
